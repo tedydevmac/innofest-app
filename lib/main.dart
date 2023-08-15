f4e2ffba7a94bfaa0e8699a37b303945c154b906
Goh Min Wen, Ted <ilovefirebase4@gmail.com>
2023-08-15 10:44:58 +0800

    Revert "Revert "HomePage UI""
    This reverts commit bed3174a57af08ed38d0eba50295789428c0d607.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:innofest_app/screens/HomePage.dart';
 
+final ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  useMaterial3: true
+);
+
+final ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  useMaterial3: true
+);
+
 void main() {
   runApp(const HomePage());
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:innofest_app/main.dart';
 import 'package:innofest_app/screens/HomePage.dart';
 
 void main() {
